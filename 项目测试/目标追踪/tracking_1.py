68102130060329464dc87c452fdf6040babc7dd1
Kenny <kenny0326@qq.com>
2022-10-21 18:44:31 +0800

    KCF追踪

---
+++ 项目测试/目标追踪/tracking_1.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# coding:utf-8
+"""
+# File       : tracking_1.py
+# Time       ：10/21/22 5:25 PM
+# Author     ：Kust Kenny
+# version    ：python 3.6
+# Description：
+"""
+import cv2
+import numpy as np
+import argparse
+
+
+OPENCV_OBJECT_TRACKERS = {
+    "kcf":cv2.TrackerKCF_create,
+    "boost": cv2.TrackerBoosting_create,
+    "csrt": cv2.TrackerCSRT_create,
+    "goturn":cv2.TrackerGOTURN_create,
+    "mil":cv2.TrackerMIL_create(),
+    "mosse":cv2.TrackerMOSSE_create
+}
+
+def init_arg():
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-v","--video",type=str,default="./video/fly_2.mp4",help="input the video or camera")
+    ap.add_argument("-t","--tracker",type=str,default="kcf",help="please choose the tracker algorithm")
+    args = vars(ap.parse_args())
+    return args
+
+if __name__ == '__main__':
+    args = init_arg()
+    cap = cv2.VideoCapture(args["video"])
+    trackers = cv2.MultiTracker_create()
+
+    _ = True
+    while _:
+        _,frame = cap.read()
+
+        (h,w) = frame.shape[:2]
+        width = 600
+        r = width/float(w)
+        frame = cv2.resize(frame,(width,int(h*r)))
+
+        (success,boxes) = trackers.update(frame)
+
+        # 绘制区域
+        try:
+            for box in boxes:
+                (x,y,w,h) = [int(v) for v in box]
+                cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+        except:
+            pass
+
+
+        cv2.imshow("press to select a box",frame)
+        key = cv2.waitKey(30) & 0xFF
+        if key == ord("s"):
+            # 选择一个区域
+            box = cv2.selectROI("press to select a box",frame,fromCenter=False,showCrosshair=True)
+
+            # 创建一个新的追踪器
+            tracker = OPENCV_OBJECT_TRACKERS[args["tracker"]]()
+            trackers.add(tracker,frame,box)
+        elif key == 27:
+            break
+    cv2.destroyAllWindows()
+    cap.release()
