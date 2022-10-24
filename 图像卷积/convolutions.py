1eaa402bf1c98ca03c0f1e99d50cf02fef1dc4cc
Kenny <kenny0326@qq.com>
2022-10-24 16:38:59 +0800

    图像卷积

---
+++ 图像卷积/convolutions.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# coding:utf-8
+"""
+# File       : convolutions.py
+# Time       ：10/24/22 4:15 PM
+# Author     ：Kust Kenny
+# version    ：python 3.6
+# Description：卷积流程
+"""
+import cv2
+
+
+def convolve(img,kenel):
+    pass
+
+if __name__ == '__main__':
+    img = cv2.imread("./pic/dog2.png",0)
